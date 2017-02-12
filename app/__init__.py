76f0b25350f30c2ab139f3d3bcd08a282be45e59
mingrammer <k239507@gmail.com>
2017-02-12 18:28:32 +0900

    Delete flask-bower extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
-from flask_bower import Bower
 from flask_pymongo import PyMongo
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-# Register bower
-Bower(app)
 
 # Create mongodb client
 mongo = PyMongo(app)
