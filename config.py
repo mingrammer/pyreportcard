57e8420c929ccb7f5ef49c8e1feff881fa7d25ab
mingrammer <k239507@gmail.com>
2017-04-19 16:08:22 +0900

    :bug: Use default values if there is no secret_config.py

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
-from config_secret import SecretConfig
-
-
 class Config:
-    SECRET_KEY = SecretConfig.SECRET_KEY
-
-    # MongoDB config
-    MONGO_DBNAME = SecretConfig.MONGO_DBNAME
-    MONGO_HOST = SecretConfig.MONGO_HOST
-    MONGO_PORT = SecretConfig.MONGO_PORT
-
+    try:
+        from config_secret import SecretConfig
+        SECRET_KEY = SecretConfig.SECRET_KEY
+        # MongoDB config
+        MONGO_DBNAME = SecretConfig.MONGO_DBNAME
+        MONGO_HOST = SecretConfig.MONGO_HOST
+        MONGO_PORT = SecretConfig.MONGO_PORT
+    except ImportError:
+        SECRET_KEY = 'local-secret'
+        # MongoDB config
+        MONGO_DBNAME = 'reportcard'
+        MONGO_HOST = 'localhost'
+        MONGO_PORT = 27017
+            
     # Cloning config
     CLONE_TMP_DIR = 'tmp'
     CLONE_TIMEOUT = 30
