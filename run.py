7416cd68c63d30dbd5b4cd72346ec42f431190aa
Xavier Hardy <xavierhardy@users.noreply.github.com>
2019-02-10 17:12:25 -0600

    (#13) Support arguments in run.py
    Fix binding to all interfaces by default (security issue)
    Support host, port, debug, and threaded

--- run.py
+++ run.py
@@ -1,4 +1,31 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+
 from app import app
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="Run a web server to analyze Python applications and report results."
+    )
+    parser.add_argument(
+        "--host", "-H", type=str, default="127.0.0.1", help="The interface to bind to."
+    )
+    parser.add_argument(
+        "--port", "-p", type=int, default=5000, help="The port to bind to."
+    )
+    parser.add_argument(
+        "--debug",
+        "-d",
+        action="store_true",
+        default=False,
+        help="Enable the debugger.",
+    )
+    parser.add_argument(
+        "--threaded",
+        "-t",
+        action="store_true",
+        default=False,
+        help="Enable multi threading.",
+    )
+    arguments = parser.parse_args()
+    app.run(**vars(arguments))
